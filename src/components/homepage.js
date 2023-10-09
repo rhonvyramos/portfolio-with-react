97327dd9c61c299b4dc2cf8f33109e5e37271b13
rhonvyramos <rhonvyramos@gmail.com>
2023-10-09 15:24:26 -0400

    added placeholder style for homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/homepage.css"
 
 export default function HomePage({selectedPage, setPage}) {
     console.log("Selected page is: " + selectedPage);
     return (
-        <h1>Placeholder Home Header</h1>
+        <h1 id="homepage_h1">Placeholder Home Header</h1>
     )
 }
